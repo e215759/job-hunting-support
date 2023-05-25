377acf0fe9d89aacde2f84e99c0694d374aec6db
tamotsu <e215759@ie.u-ryukyu.ac.jp>
2023-05-25 13:08:56 +0900

    Initial Commit

---
+++ job-hunting-support/job_hunting_supportApp.swift
@@ -0,0 +1,17 @@
+//
+//  job_hunting_supportApp.swift
+//  job-hunting-support
+//
+//  Created by 宮平保 on 2023/05/25.
+//
+
+import SwiftUI
+
+@main
+struct job_hunting_supportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
