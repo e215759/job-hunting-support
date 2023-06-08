91ad9506a6f9785ee565a70367b41af6bb861eb8
tamotsu <e215759@ie.u-ryukyu.ac.jp>
2023-06-08 15:28:48 +0900

    練習用でアイコン用の写真を追加した。 FirstViewからContentViewに画面遷移させたいんだけど思うように動かないので、今後さらに訂正を加えていく予定。

--- job-hunting-support/FirstView.swift
+++ job-hunting-support/FirstView.swift
@@ -9,90 +9,31 @@ import SwiftUI
 
 struct FirstView: View {
     
-    @State private var isButtonPressed = false
+    @State private var pushSignUp = false
     
     var body: some View {
-        VStack {
-            // 他のコンテンツを追加
-            
-            Button("ボタン") {
-                isButtonPressed = true
-            }
-            .padding()
-            .background(Color.blue)
-            .foregroundColor(.white)
-            .cornerRadius(10)
-        }
-        .sheet(isPresented: $isButtonPressed) {
-            ContentView()
-        }
-    }
-    
-    /*
-    var body: some View {
-        VStack {
-            // 他のコンテンツを追加
-            
-            Button("ボタン") {
-                isButtonPressed = true
-            }
-            .padding()
-            .background(Color.blue)
-            .foregroundColor(.white)
-            .cornerRadius(10)
-        }
-        .sheet(isPresented: $isButtonPressed) {
-            ContentView()
-        }
-        /*
         NavigationView {
             VStack {
-                // 他のコンテンツを追加
-                
-                NavigationLink(destination: ContentView(), isActive: $isButtonPressed) {
-                    EmptyView()
+                // 画面遷移を行うボタン
+                NavigationLink(destination: ContentView()) {
+                    Text("画面遷移ボタン")
                 }
                 
-                Button("ボタン") {
-                    isButtonPressed = true
-                }
-                .padding()
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .cornerRadius(10)
+                // その他のビューコンテンツ
+                // ...
             }
         }
-         */
     }
-            /*
-            // 他のコンテンツを追加
-            Button("ボタン") {
-                isButtonPressed = true
-            }
-            .padding()
-            .background(Color.blue)
-            .foregroundColor(.white)
-            .cornerRadius(10)
-            
-            // 遷移先のViewを表示
-            if isButtonPressed {
-                ContentView()
-            }
-             */
-        /*
-        HStack{
-            // ユーザー情報入力用のビューを構築します
-            Text("Welcome! Create your account.")
-            Button("次へ") {
-                // 遷移先のViewを表示する処理を書く
-                ContentView()
-            }
-            .padding()
-            .background(Color.blue)
-            .foregroundColor(.white)
-            .cornerRadius(10)
-        }
-         */
-    }
-     */
 }
+
+struct FirstView_Previews: PreviewProvider {
+   static var previews: some View {
+       FirstView()
+   }
+}
+
+
+
+
+
+
